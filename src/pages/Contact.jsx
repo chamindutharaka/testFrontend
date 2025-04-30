cac6057b7debe33ef8c5a61ed6a36d6c8edabe4d
Yasas123-cmyk <it22324160@my.sliit.lk>
2025-04-30 22:27:58 +0530

    first

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,7 @@ const Contact = () => {
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col items-start justify-center gap-6'>
           <p className='text-lg font-semibold text-gray-600 '>OUR Main OFFICE</p>
-          <p className='text-gray-500 '>113/D Highlevel Rd, <br /> Colombo 03, Sri Lanka</p>
+          <p className='text-gray-500 '>113/D Highlevel Rd, <br /> Colombo 07, Sri Lanka</p>
           <p className='text-gray-500 '>Tel: +94-112-456-7890 <br /> Email: bookmydoc@info.com</p>
           <p className='text-lg font-semibold text-gray-600 '>CAREERS AT BookMyDoc</p>
           <p className='text-gray-500 '>Learn more about our teams and job openings.</p>
