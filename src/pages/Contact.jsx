6223539e41665ba0882f4619132249146f3211e3
Yasas123-cmyk <it22324160@my.sliit.lk>
2025-04-30 22:23:52 +0530

    change

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
       <div className='flex flex-col justify-center gap-10 my-10 text-sm md:flex-row mb-28'>
         <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
         <div className='flex flex-col items-start justify-center gap-6'>
-          <p className='text-lg font-semibold text-gray-600 '>OUR OFFICE</p>
+          <p className='text-lg font-semibold text-gray-600 '>OUR Main OFFICE</p>
           <p className='text-gray-500 '>113/D Highlevel Rd, <br /> Colombo 03, Sri Lanka</p>
           <p className='text-gray-500 '>Tel: +94-112-456-7890 <br /> Email: bookmydoc@info.com</p>
           <p className='text-lg font-semibold text-gray-600 '>CAREERS AT BookMyDoc</p>
